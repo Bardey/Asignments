0a91c079bed631f7c120245dd04a40583b0a15c7
Bardey <38652223+Bardey@users.noreply.github.com>
2022-08-01 12:50:57 +0200

    Update ddlqueries.sql

--- Asessment/ddlqueries.sql
+++ Asessment/ddlqueries.sql
@@ -1,6 +1,97 @@
 CREATE TYPE status AS ENUM ('completed', 'in progress');
 
+DROP FUNCTION IF EXISTS unique_checker CASCADE;
+CREATE FUNCTION unique_checker() 
+   RETURNS TRIGGER 
+   LANGUAGE PLPGSQL
+   AS $$
+BEGIN
+  IF NEW.user_id IN (SELECT user_id FROM employee) 
+  OR NEW.username IN (SELECT username FROM employee) 
+  OR NEW.employee_num IN (SELECT employee_num FROM employee) THEN
+  RAISE NOTICE 'This record exists already';
+  RETURN NULL;
+  END IF;
+  RETURN NEW;
+END;
+$$;
 
+-- TRIGGER 
+DROP TRIGGER IF EXISTS unique_checker ON employee;
+CREATE TRIGGER unique_checker
+BEFORE INSERT ON employee
+FOR EACH ROW 
+EXECUTE PROCEDURE unique_checker();
+
+
+
+DROP FUNCTION IF EXISTS unique_checker2 CASCADE;
+CREATE FUNCTION unique_checker2() 
+   RETURNS TRIGGER 
+   LANGUAGE PLPGSQL
+   AS $$
+BEGIN
+  IF NEW.course_id IN (SELECT course_id FROM course) 
+  OR NEW.course_name IN (SELECT course_name FROM course) 
+  OR NEW.platform_id IN (SELECT platform_id FROM course) THEN
+  RAISE NOTICE 'This record exists already';
+  RETURN NULL;
+  END IF;
+  RETURN NEW;
+END;
+$$;
+
+-- TRIGGER 
+DROP TRIGGER IF EXISTS unique_checker2 ON employee;
+CREATE TRIGGER unique_checker2
+BEFORE INSERT ON course
+FOR EACH ROW 
+EXECUTE PROCEDURE unique_checker2();
+
+
+DROP FUNCTION IF EXISTS unique_checker_platform CASCADE;
+CREATE FUNCTION unique_checker_platform() 
+   RETURNS TRIGGER 
+   LANGUAGE PLPGSQL
+   AS $$
+BEGIN
+  IF NEW.platform_id IN (SELECT platform_id FROM platform) 
+  OR NEW.platform_name IN (SELECT platform_name FROM platform)  THEN
+  RAISE NOTICE 'This record exists already';
+  RETURN NULL;
+  END IF;
+  RETURN NEW;
+END;
+$$;
+
+-- TRIGGER 
+DROP TRIGGER IF EXISTS unique_checker_platform ON employee;
+CREATE TRIGGER unique_checker_platform
+BEFORE INSERT ON platform
+FOR EACH ROW 
+EXECUTE PROCEDURE unique_checker_platform();
+
+
+DROP FUNCTION IF EXISTS unique_checker_ongoing_training CASCADE;
+CREATE FUNCTION unique_checker_ongoing_training() 
+   RETURNS TRIGGER 
+   LANGUAGE PLPGSQL
+   AS $$
+BEGIN
+  IF NEW.training_id IN (SELECT training_id FROM ongoing_training) THEN
+  RAISE NOTICE 'This record exists already';
+  RETURN NULL;
+  END IF;
+  RETURN NEW;
+END;
+$$;
+
+-- TRIGGER 
+DROP TRIGGER IF EXISTS unique_checker_ongoing_training ON employee;
+CREATE TRIGGER unique_checker_ongoing_training
+BEFORE INSERT ON ongoing_training
+FOR EACH ROW 
+EXECUTE PROCEDURE unique_checker_ongoing_training();
 
 DROP TABLE employee;
 CREATE TABLE employee (
@@ -21,7 +112,7 @@ CREATE TABLE course (
 	duration TIME NOT NULL,
 	create_date DATE NOT NULL,
 	tags TEXT NOT NULL,
-	photo TEXT NOT NULL,
+	photo JSONB,
 	is_active BOOL DEFAULT '1'
 );
 DROP TABLE platform;
@@ -60,7 +151,7 @@ CREATE TABLE ongoing_training (
 	status STATUS NOT NULL,
 	complete_percentage DECIMAL(3,2) NOT NULL,
 	start_date DATE NOT NULL,
-	finish_date DATE NOT NULL,
+	finish_date DATE,
 	last_updated DATE NOT NULL,
 	is_active BOOL DEFAULT '1'
 );
